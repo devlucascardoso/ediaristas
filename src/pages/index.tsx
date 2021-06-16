5eb0c156b73ccab50f317a3e755c7377e1dfb8da
Lucas Cardoso <lucascardoso946@gmail.com>
2021-06-16 17:47:48 -0300

    Finalizando Frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import SafeEnvironment from "ui/components/feedBack/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -9,21 +16,39 @@ export default function Home() {
       <PageTitle
         title={"Conheça os profissionais"}
         subtitle={
-          "Preencha seu endereço e veja todos os profissionais da sua área"
+          "Preencha seu endereço e veja todos os profissionais da sua localização"
         }
       />
-      <UserInformation
-        name={"Lucas Cardoso"}
-        picture={"https://github.com/devlucascardoso.png"}
-        rating={3}
-        description={"Rio Grande do Norte"}
-      />
-      <UserInformation
-        name={"Stenio Wagner"}
-        picture={"https://github.com/steniowagner.png"}
-        rating={3}
-        description={"Ceará"}
-      />
+
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"999.999-99"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>CEP Inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Lucas Cardoso"}
+              picture={"https://github.com/devlucascardoso.png"}
+              rating={3}
+              description={"Rio Grande do Norte"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
